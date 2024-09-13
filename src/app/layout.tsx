9b3729dc10152137b3ece1efa4a7619efb9721da
Nikhil Rathore <nikhilrathore52297@gmail.com>
2024-09-13 21:16:52 +0530

    Complete Project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="relavtive w-full flex items-center justify-center">
+        <div className="relative w-full flex items-center justify-center">
           <Navbar />
         </div>
         {children}
