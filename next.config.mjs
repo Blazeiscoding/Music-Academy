c7d719c6607d921f577f02bfc3a2bba7557ac53a
Nikhil Rathore <nikhilrathore52297@gmail.com>
2024-09-14 01:34:44 +0530

    Bug Fix Final

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,7 @@ const nextConfig = {
     },
     experimental: {
     appDir: false,
-  }
-
+    },
 }
 
 
--- src/components/ui/Spotlight.tsx
+++ src/components/ui/Spotlight.tsx
@@ -1,5 +1,5 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+"use client";
+
 import React from "react";
 import { cn } from "@/utils/cn";
 
